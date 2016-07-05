185d86d12a673c5514422328f1deb172002f2ce6
HelloTommy <875318524@qq.com>
2016-07-05 22:20:51 +0800

    test

--- app/src/main/java/com/qianfen/mysvntest/MainActivity.java
+++ app/src/main/java/com/qianfen/mysvntest/MainActivity.java
@@ -3,17 +3,19 @@ package com.qianfen.mysvntest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getName();
+    private TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "onCreate: jk");
-        Log.d(TAG, "onCreate: jk");
-        Log.d(TAG, "onCreate: jk");
+
+        textView = (TextView) findViewById(R.id.main_text);
     }
 }
